6d504464349c0255f875478ffe1da5419c0fe632
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-12 14:32:53 +0300

    Video Viewer with JS

--- DayEleven/script.js
+++ DayEleven/script.js
@@ -21,12 +21,36 @@ function skip() {
     console.log(this.dataset.skip);
     video.currentTime += parseFloat(this.dataset.skip);
 }
-
+function handleRangeUpdate() {
+    video[this.name] = this.value
+    console.log(this.name)
+    console.log(this.value)
+}
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100
+    progressBar.style.flexBasis = `${percent}%`
+}
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(scrubTime)
+}
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 
+let mouseDown = false;
+progress.addEventListener('mousemove', (e) => {
+    if (mouseDown) {
+        scrub();
+    } 
+});
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousedown', () => mouseDown = true)
+progress.addEventListener('mouseup', () => (mouseDown = false));
