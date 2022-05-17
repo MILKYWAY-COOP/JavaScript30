595765465c0f547fc5358074f6cebd9d280ecb03
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-17 07:57:47 +0300

    Notes for the challenge

---
+++ DaySixteen/main.js
@@ -0,0 +1,10 @@
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+
+function shadow(e) {
+    const { offsetWidth: width, offsetHeight: height } = hero
+    let { offsetX: x, offsetY: y } = e;
+
+    console.log(this, e.target)
+}
+hero.addEventListener('mousemove', shadow);
