6ddbe6025f69746cd44f0c5191636c9fe43dff1d
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-20 10:48:31 +0300

    Done with the filters

--- DayNineteen/scripts.js
+++ DayNineteen/scripts.js
@@ -1,8 +1,8 @@
+const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('.2d');
+const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
-const video = document.querySelector('.player');
 
 function getVideo() {
   navigator.mediaDevices
@@ -11,6 +11,7 @@ function getVideo() {
       audio: false
     })
     .then((localMediaStream) => {
+      console.log(localMediaStream);
       video.srcObject = localMediaStream;
       video.play();
     })
@@ -18,8 +19,6 @@ function getVideo() {
       console.error('OH No!!!!', err);
     });
 }
-
-getVideo();
 
 function paintToCanvas() {
   const width = video.videoWidth;
@@ -29,10 +28,81 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    //pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.1;
+
+    pixels = greenScreen(pixels);
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
 function takePhoto() {
-    snap.currentTime = 0;
-    snap.play();
+  // Played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  //take the data out of the canvas
+  const data = canvas.toDataURL('images/jpeg');
+  const newTag = document.createElement('a');
+  newTag.href = data;
+  newTag.setAttribute('download', 'handsome');
+  newTag.textContent = 'Download Image';
+  newTag.innerHTML = `<img src="${data}" alt="Simple Boy" />`;
+  strip.insertBefore(newTag, strip.firstChild);
 }
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] - 100; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
