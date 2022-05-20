b31b363d232d8d67cd3069ae5e635e41eafc37d1
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-20 09:22:26 +0300

    Working with audio

--- DayNineteen/scripts.js
+++ DayNineteen/scripts.js
@@ -1,21 +1,38 @@
-const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('.2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+const video = document.querySelector('.player');
 
 function getVideo() {
-    navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: false
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: false
     })
-        .then(localMediaStream => {
-            video.srcObject = localMediaStream;
-            video.play();
-        })
-        .catch(err => {
-        console.error('OH No!!!!', err)
+    .then((localMediaStream) => {
+      video.srcObject = localMediaStream;
+      video.play();
     })
+    .catch((err) => {
+      console.error('OH No!!!!', err);
+    });
 }
 
-getVideo()
+getVideo();
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+}
