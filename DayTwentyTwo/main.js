3236dd858a751b26161277d19f7b17621434580e
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-23 19:44:58 +0300

    Highlighter

--- DayTwentyTwo/main.js
+++ DayTwentyTwo/main.js
@@ -7,6 +7,16 @@ document.body.append(highLight);
 function highLightlink() {
     const linkCoords = this.getBoundingClientRect();
     console.log(linkCoords);
+    const coords = {
+      width: linkCoords.width,
+        height: linkCoords.height, 
+      top: linkCoords.top + window.scrollY,
+      left: linkCoords.left + window.scrollX,
+    };
+
+    highLight.style.width = `${coords.width}px`;
+    highLight.style.height = `${coords.height}px`;
+    highLight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
 }
 
 triggers.forEach((a) => a.addEventListener('mouseenter', highLightlink));
