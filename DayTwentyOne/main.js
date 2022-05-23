9903fbc45ad2aad260cf23d024c6dc99179a5bf5
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-23 18:48:29 +0300

    Geolocation finder

--- DayTwentyOne/main.js
+++ DayTwentyOne/main.js
@@ -1,1 +1,8 @@
 /*Compass: https://thenounproject.com/search/?q=compass&i=592352*/
+
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition((data) => {
+  console.log(data);
+});
