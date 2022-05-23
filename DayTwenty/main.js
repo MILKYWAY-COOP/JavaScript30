0158ec887e355445c7c37a377bde63244fc4e5e0
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-23 18:21:36 +0300

    Finale!

--- DayTwenty/main.js
+++ DayTwenty/main.js
@@ -8,6 +8,22 @@ let p = document.createElement('p');
 const words = document.querySelector('.words');
 words.appendChild(p);
 
-recognition.addEventListener('results', e => {
-    console.log(e);
+recognition.addEventListener('result', (e) => {
+  const transcript = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join('');
+
+    p.textContent = transcript;
+    if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+    if (transcript.includes('get the weather')) {
+        console.log('Getting the weather');
+    }
 });
+
+recognition.addEventListener('end', recognition.start)
+
+recognition.start();
