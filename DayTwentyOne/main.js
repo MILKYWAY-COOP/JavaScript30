4154a13d8d664354061b4beee970901c2201f3c4
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-23 18:55:18 +0300

    Geolocation finder

--- DayTwentyOne/main.js
+++ DayTwentyOne/main.js
@@ -4,5 +4,10 @@ const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
 
 navigator.geolocation.watchPosition((data) => {
-  console.log(data);
+    console.log(data);
+    speed.textContent = Math.round(data.coords.speed);
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+}, (err) => {
+    console.err(err);
+    alert("Hey!! You have to allow that to happen")
 });
