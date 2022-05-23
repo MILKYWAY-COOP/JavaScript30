46adaabc9918aa60cbe6990fb37b88c7dbaa1554
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-23 18:04:33 +0300

    Added server files

--- DayTwenty/main.js
+++ DayTwenty/main.js
@@ -8,4 +8,6 @@ let p = document.createElement('p');
 const words = document.querySelector('.words');
 words.appendChild(p);
 
-recognition.addEventListener('results', e => {} )
+recognition.addEventListener('results', e => {
+    console.log(e);
+});
