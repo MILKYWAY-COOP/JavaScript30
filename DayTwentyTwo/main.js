4cceb38ce59ebf7929058d2e2e036d412a65f8e2
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-23 19:30:30 +0300

    BoundingClients

--- DayTwentyTwo/main.js
+++ DayTwentyTwo/main.js
@@ -2,4 +2,11 @@ const triggers = document.querySelectorAll('a');
 const highLight = document.createElement('span');
 
 highLight.classList.add('highLight');
-document.body.append('highLight');
+document.body.append(highLight);
+
+function highLightlink() {
+    const linkCoords = this.getBoundingClientRect();
+    console.log(linkCoords);
+}
+
+triggers.forEach((a) => a.addEventListener('mouseenter', highLightlink));
