6d7063fe1f4e7fffbcfe335b9f1ce6e3c93ddcc6
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-24 20:00:27 +0300

    Speech Synthesis

--- DayTwentyThree/main.js
+++ DayTwentyThree/main.js
@@ -1,6 +1,18 @@
 const msg = new SpeechSynthesisUtterance();
 let voices = [];
-const voicesDropdown = document.querySelector('[name="voice"]');
-const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakButton = document.querySelector('#speak');
-const stopButton = document.querySelector('#stop');
+const voicesDropdown =
+    document.querySelector('[name="voice"]');
+const options =
+    document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton =
+    document.querySelector('#speak');
+const stopButton =
+    document.querySelector('#stop');
+msg.text = document.querySelector('[name=text]').value
+
+function populateVoices() {
+    voices = this.getVoices()
+    console.log(voices);
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
