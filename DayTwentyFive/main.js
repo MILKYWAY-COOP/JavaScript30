6df3e994addbdbeacece4edf09e76d5a6433cd9c
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-27 08:19:23 +0300

    Propagation, Bubbling, Capture & Once

--- DayTwentyFive/main.js
+++ DayTwentyFive/main.js
@@ -1,7 +1,18 @@
 const divs = document.querySelectorAll('div');
+const button = document.querySelector('button')
 
 function logText(e) {
-  console.log(this);
+    console.log(this.classList.value);
+    // e.stopPropagation() //stop propagation
 }
 
-divs.forEach((div) => div.addEventListener('click', logText));
+divs.forEach((div) => div.addEventListener('click', logText, {
+    capture: false,
+    once: true
+}));
+
+button.addEventListener('click', () => {
+    console.log('Click!!')
+}, {
+    once: true
+})
