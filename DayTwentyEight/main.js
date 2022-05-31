fce26e2118cdd5f5ab2f1f55b83b0e3068445c36
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-31 12:01:52 +0300

    Starter Files

---
+++ DayTwentyEight/main.js
@@ -0,0 +1,7 @@
+const speed = document.querySelector('.speed')
+const bar = speed.querySelector('.speed-bar')
+const videos = document.querySelector('.flex')
+
+speed.addEventListener('mousemove', function (e) {
+    console.log(e)
+})
