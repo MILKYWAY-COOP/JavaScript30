3f2ccb074c6cbb4b4c5191ede731964fa5131817
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-02 07:39:53 +0300

    Countdown Clock

--- DayTwentyNine/script.js
+++ DayTwentyNine/script.js
@@ -1,10 +1,15 @@
 let countDown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  //clear any existing timers
+  clearInterval(countDown);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countDown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -24,4 +29,25 @@ function displayTimeLeft(seconds) {
     remainderSeconds < 10 ? '0' : ''
   }${remainderSeconds}`;
   timerDisplay.textContent = display;
+  document.title = display;
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${hour}:${minutes}hrs`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach((button) => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  timer(mins * 60)
+  this.reset();
+});
